7943942fe739faae727381ea0abdfa2cd0e03501
JulianBlohm <julianscript@outlook.de>
2021-01-28 15:52:09 +0100

    Change grid layout

--- src/App.js
+++ src/App.js
@@ -17,12 +17,13 @@ function App() {
 
     return (
         <Showcase>
+            <Heading>Weather at Closelink</Heading>
             <CurrentWeatherGrid>
+                {weather.icon && <Icon src={weatherIconUrl} alt="" />}
                 <Temperature>{weather.temp}°</Temperature>
-                {weather.icon && <img src={weatherIconUrl} alt="" />}
-                <h2>{weather.description}</h2>
+                <Description>{weather.description}</Description>
                 {weather.wind > 0 ? (
-                    <h2>Wind {weather.wind} m/s</h2>
+                    <Wind>Wind {weather.wind} m/s</Wind>
                 ) : (
                     <h2>No wind</h2>
                 )}
@@ -35,16 +36,37 @@ const Showcase = styled.main`
     background: var(--main-blue);
     height: 100vh;
     color: white;
+    text-align: center;
 `
 
 const CurrentWeatherGrid = styled.article`
     display: grid;
-    grid-template: 50% 50% / 50% 50%;
+    grid-template: auto / 30% 40% 30%;
     place-items: center;
 `
 
-const Temperature = styled.h1`
+const Heading = styled.h1`
+    padding: 20px 0;
+`
+
+const Icon = styled.img`
+    grid-column: 2 / 3;
+`
+
+const Temperature = styled.h2`
     font-size: 6rem;
+    grid-column: 1 / -1;
+    margin: 50px 0;
+`
+
+const Description = styled.span`
+    grid-column: 1 / 2;
+    grid-row: 1 / 2;
+`
+
+const Wind = styled.span`
+    grid-column: 3 / 4;
+    grid-row: 1 / 2;
 `
 
 export default App
