3fd7b8470d179a25e8bb2248850bfda4539d766d
JulianBlohm <julianscript@outlook.de>
2021-01-28 12:26:25 +0100

    Add api call logic to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
+import getWeatherData from './services/getWeatherData'
+
 function App() {
-  return (
-    <div>
-      <h1>Test</h1>
-    </div>
-  );
+    const [weatherInformation, setWeatherInformation] = useState()
+    
+    useEffect(() => loadWeatherInformation(), [])
+
+    async function loadWeatherInformation() {
+        const loadedData = await getWeatherData()
+        loadedData === 'error'
+            ? console.log('Weather fetch error')
+            : loadIcon(loadedData.icon) && setWeatherInformation(loadedData)
+    }
+
+    async function loadIcon() {
+        const loadedIcon = await getWeatherIcon()
+        loadedIcon === 'error' ? console.log('Icon fetch error') : showWeather()
+    }
+
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
